127ae43b234e075b36bb6afce664373eb8ed1ed4
muskan <muskanloach984@gmail.com>
2024-04-16 23:59:27 +0530

    File system

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ fs.writeFile("heyy.txt", "hello backend" , function(err){
 if(err) console.error(err);
 else console.log("done");
 })
+
+
+
+fs.appendFile('heyy.txt', 'welcome to backend', (err) => {
+  if (err) throw err;
+  console.log('The "data to append" was appended to file!');
+});
+
+
+
+fs.rename('heyy.txt', 'hiii.txt', (err) => {
+  if (err) throw err;
+  console.log('Rename complete!');
+});
